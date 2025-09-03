9066854943a64fd0950348a296cd4b3ac00a42c1
kassandrabeas5 <kassandrabeas4@gmail.com>
2025-09-02 20:56:28 -0700

    feat(job-list): visual skeleton + minimal tests; include app.json and tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,4 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
@@ -40,6 +39,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen 
+        name="jobs" 
+        options={{ 
+          title: "Jobs" 
+        }} 
+       />
+
     </Tabs>
   );
 }
