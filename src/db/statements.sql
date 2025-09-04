852a6bf84f29afe30915b8dc67247c735b621ebd
connorobro <connorobro@gmail.com>
2025-09-03 18:53:33 -0700

    added job table with job_id, title, and company and a table of saved jobs where each saved job is attributed to a specific user id

--- src/db/statements.sql
+++ src/db/statements.sql
@@ -6,3 +6,19 @@ CREATE TABLE IF NOT EXISTS users (
   password_hash TEXT NOT NULL,
   created_at INTEGER DEFAULT (strftime('%s','now'))
 );
+
+CREATE TABLE IF NOT EXISTS jobs (
+  job_id TEXT PRIMARY KEY,
+  title TEXT,
+  company TEXT
+);
+
+CREATE TABLE IF NOT EXISTS saved_jobs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  job_id TEXT NOT NULL,
+  saved_at INTEGER DEFAULT (strftime('%s','now')),
+  UNIQUE(user_id, job_id),
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY(job_id) REFERENCES jobs(job_id) ON DELETE CASCADE
+);
