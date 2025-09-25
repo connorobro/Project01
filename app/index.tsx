1989c2c6d8c8dc39e9d0765b12404c3fa95c7fb5
Rene Vega <renev2013@gmail.com>
2025-09-24 19:09:24 -0700

    Added more styling to index.tsx page

--- app/index.tsx
+++ app/index.tsx
@@ -15,45 +15,49 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Job Listing</Text>
-      <Link href="./Register" style={styles.button}>
-        Register
-      </Link>
-      <Link href={"./Login"} style={styles.button}>
-        Login
-      </Link>
-      {/* <Link href={"/Home"} style={styles.button}>
-        Home
-      </Link>
-      <Link href={"/savedJobs"} style={styles.button}>
-        Saved Jobs
-      </Link>
-      <Link href={"/jobs"} style={styles.button}>
-        Job
-      </Link> */}
-      {/* <Link href={"/debug"} style={styles.button}>
-        View Database
-      </Link> */}
+        <view style={{width:'25%',}}>
+          <Text style={styles.title}>Job Listing App</Text>
+        </view>
+      <View style={{flexDirection: 'column',justifyContent:'space-evenly', width: '50%',height: '45%', backgroundColor: '#003459', borderRadius: 15}}>
+        <Link href="./Register" style={styles.button}>
+          Register
+        </Link>
+        <Link href={"./Login"} style={styles.button}>
+          Login
+        </Link>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
-    backgroundColor: "#25292e",
+    // backgroundColor: "#25292e",
+    backgroundColor: '#00171F',
+    // backgroundColor: '#00171F',
+    textAlign: 'center'
   },
   text: {
     color: "white",
+    fontWeight: 'bold',
+    fontSize: 25,
+    // marginBottom: 100
   },
   button: {
+    textAlign: 'center',
     fontSize: 20,
-    margin: 10,
+    margin: 15,
     backgroundColor: "#007AFF",
     borderRadius: 10,
     padding: 15,
     // textDecorationLine: "underline",
     color: "#fff",
   },
+  title:{
+    fontSize: 40,
+    color: '#007EA7',
+    fontWeight: '800'
+  }
 });
